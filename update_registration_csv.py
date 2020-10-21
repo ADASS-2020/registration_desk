47c245cf7fc463f4d75ed5fff2ef005f89bbf155
Jose Enrique Ruiz <jer@iaa.es>
2020-10-21 19:58:39 +0200

    add info from pretalx on speakers and posters

--- update_registration_csv.py
+++ update_registration_csv.py
@@ -3,6 +3,7 @@ import os
 import shutil
 import time
 import gspread
+import psycopg2
 
 
 FNAME = 'registration.csv'
@@ -13,7 +14,7 @@ FIELDS = {
     'ticket_id': lambda r: r['Reference'],
     'isspeaker': lambda r: r['SPEAKER'] == 'Yes',
     'istrainer': lambda r: r['TRAINER'] == 'Yes',
-    'isposterauth': lambda r: r['POSTER AUTHOR'] == 'Yes',
+    'isposterauth': lambda r: r['POSTER'] == 'Yes',
     'isadmin': lambda r: r['ADMIN'] == 'Yes',
     'isloc': lambda r: r['LOC'] == 'Yes',
     'ispoc': lambda r: r['POC'] == 'Yes',
@@ -28,6 +29,33 @@ def backup_existing_file(fname):
         bkp_name = f'{fname}-{time.time()}'
     shutil.copyfile(fname, bkp_name)
 
+
+conn = psycopg2.connect(database="pretalx",
+                        user="pretalx",
+                        password="",
+                        host="localhost",
+                        port="5432")
+
+
+sql = """
+SELECT 
+submission_submission.title, 
+submission_submission.submission_type_id,
+submission_submission.paper_id, 
+person_user.email
+FROM 
+submission_submission,
+person_user
+WHERE
+submission_submission.main_author_id=person_user.id
+AND submission_submission.state not in ('deleted', 'withdrawn')
+ORDER BY email;
+"""
+cur = conn.cursor()
+cur.execute(sql)
+records = cur.fetchall()
+type_ids = [row[1] for row in records]
+authors_emails = [row[3] for row in records]
 
 gc = gspread.service_account()
 wks = gc.open('ADASS XXX Registrations').sheet1
@@ -53,6 +81,16 @@ with open(FNAME, 'w', newline='') as csvfile:
         if not rec['Reference']:
             missing += 1
             continue
+
+        # fetch speakers and posters authors
+        if rec['Email'] and rec['Email'] in authors_emails:
+            idx = authors_emails.index(rec['Email'])
+            type_id = type_ids[idx]
+            if type_id == 13 or type_id == 18:
+                rec['POSTER'] = "Yes"
+            else:
+                rec['SPEAKER'] = "Yes"
+
         # Make sure to strip all values!
         for k in rec:
             if isinstance(rec[k], str):
@@ -61,3 +99,6 @@ with open(FNAME, 'w', newline='') as csvfile:
         writer.writerow({k: fn(rec) for k, fn in FIELDS.items()})
         n += 1
 print(f'Written {n} records, missing {missing}')
+
+conn.close()
+
